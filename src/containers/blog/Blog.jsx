02a9f3c743645c49bcea6d7248d1c77e79a285fb
jerrvon <jerrvonewing.cs@gmail.com>
2024-02-26 21:15:16 -0600

    Created the Blog/Article components/containers with custom styling

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+
+import { Article } from '../../components';
+import {blogs} from './imports';
 import './Blog.css';
 
+
 const Blog = () => {
+
   return (
-    <div>Blog</div>
+    <div className='gpt3__blog section__padding' id='blog'>
+      <div className='gpt3__blog-heading'>
+        <h1 className='gradient__text'>A lot is happening, We are blogging about it.</h1>
+        <div className='gpt3__blog-container'>
+          <div className='gpt3__blog-container_groupA'>
+              <Article imgUrl={blogs[0].imgUrl} date={blogs[0].date} title={blogs[0].title}/>
+          </div>
+          <div className='gpt3__blog-container_groupB'>
+            {blogs.slice(1).map((blog, index) => (
+                <Article key={index} imgUrl={blog.imgUrl} date={blog.date} title={blog.title}/>
+              ))}
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
---
+++ src/containers/blog/imports.js
@@ -0,0 +1,31 @@
+import blog01Img from '../../assets/blog01.png';
+import blog02Img from '../../assets/blog02.png';
+import blog03Img from '../../assets/blog03.png';
+import blog04Img from '../../assets/blog04.png';
+import blog05Img from '../../assets/blog05.png';
+
+const blogs = [{
+    imgUrl: blog01Img,
+    date: 'Sep 26, 2021',
+    title: 'GPT-3 and Open  AI is the future. Let us exlore how it is?'},
+    {
+        imgUrl: blog02Img,
+        date: 'Sep 26, 2021',
+        title: 'GPT-3 and Open  AI is the future. Let us exlore how it is?'},
+    {
+        imgUrl: blog03Img,
+        date: 'Sep 26, 2021',
+        title: 'GPT-3 and Open  AI is the future. Let us exlore how it is?'},
+    {
+        imgUrl: blog04Img,
+        date: 'Sep 26, 2021',
+        title: 'GPT-3 and Open  AI is the future. Let us exlore how it is?'},
+    {
+        imgUrl: blog05Img,
+        date: 'Sep 26, 2021',
+        title: 'GPT-3 and Open  AI is the future. Let us exlore how it is?'}
+        
+];
+export {
+    blogs
+}
